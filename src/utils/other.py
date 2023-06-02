94b2ff1cf06fc69cdaa9db25517eedeb1e24212c
Karol Zlot <11590960+karolzlot@users.noreply.github.com>
2023-06-02 11:36:43 +0200

    Add create_pr_blueprints_from_branches() and find_branches_file()

---
+++ src/utils/other.py
@@ -0,0 +1,28 @@
+"""create PullRequestBlueprints from a list of Branches"""
+
+from src.models.types import Branch, PullRequestBlueprint
+from src.utils.read_files import load_branches_from_file
+from pathlib import Path
+import questionary as q
+from loguru import logger
+from src.config.env_vars import BRANCH_PREFIX
+
+def create_pr_blueprints_from_branches(branches: list[Branch]) -> list[PullRequestBlueprint]:
+    """Create PullRequestBlueprints from a list of Branches."""
+    pr_blueprints = []
+    # assume that branches are sorted, so former is the target of the latter
+
+    for i in range(len(branches)):
+        if i == 0:
+            assert branches[i].startswith(BRANCH_PREFIX) or branches[i] == "main"
+            continue
+        else:
+            assert branches[i].startswith(BRANCH_PREFIX)
+            pr_blueprints.append(PullRequestBlueprint(
+                head=branches[i],
+                base=branches[i - 1],
+                title=branches[i],
+            ))
+    
+    return pr_blueprints
+
--- src/utils/read_files.py
+++ src/utils/read_files.py
@@ -1,26 +1,26 @@
 from src.models.types import PRData, Branch
 from pathlib import Path
 from ruamel.yaml import YAML
+from loguru import logger
+import questionary as q
 
+# def read_prs_config_file() -> list[PRData]:
+#     file_path = Path('prs.yaml')
+#     with open(file_path, 'r') as file:
+#         yaml = YAML(typ='safe')
+#         prs_data: list[dict] = yaml.load(file)
 
+#     pull_requests_list = []
+#     for entry in prs_data:
+#         pull_request = PRData(
+#             branch=entry['branch'],
+#             title=entry['title'],
+#             pr_number=entry['pr_number'],
+#             target=entry['target']
+#         )
+#         pull_requests_list.append(pull_request)
 
-def read_prs_config_file() -> list[PRData]:
-    file_path = Path('prs.yaml')
-    with open(file_path, 'r') as file:
-        yaml = YAML(typ='safe')
-        prs_data: list[dict] = yaml.load(file)
-
-    pull_requests_list = []
-    for entry in prs_data:
-        pull_request = PRData(
-            branch=entry['branch'],
-            title=entry['title'],
-            pr_number=entry['pr_number'],
-            target=entry['target']
-        )
-        pull_requests_list.append(pull_request)
-
-    return pull_requests_list
+#     return pull_requests_list
 
 
 def load_branches_from_file(file: Path) -> list[Branch]:
@@ -36,5 +36,20 @@ def load_branches_from_file(file: Path) -> list[Branch]:
     return branches
 
 
-        
+def find_branches_file() -> Path:
+    """Return file from directory, if more than one file, ask user to select one."""
+    BRANCHES_DIR = Path("branches/")
+
+    files = list(BRANCHES_DIR.glob("*.txt"))
+    if len(files) == 0:
+        raise Exception("No files found in branches/ directory.")
+    elif len(files) == 1:
+        logger.info(f"Using {files[0]}")
+        return files[0]
+    else:
+        options = [file.name for file in files]
+        file_name = q.select("Select file with branches:", choices=options).ask()
+        logger.info(f"Using {file_name}")
+        return BRANCHES_DIR / file_name
+
 

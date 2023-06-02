e92897bb099cd1256d084d1482474078b63206db
Karol Zlot <11590960+karolzlot@users.noreply.github.com>
2023-06-02 11:19:31 +0200

    Improve validation of environment variables

--- src/config/env_vars.py
+++ src/config/env_vars.py
@@ -3,16 +3,18 @@ import os
 
 load_dotenv()
 
-github_access_token= os.getenv('GITHUB_ACCESS_TOKEN')
-local_repo_path= os.getenv('LOCAL_REPO_PATH')
-github_repo = os.getenv('GITHUB_REPO')
+def get_env(name: str) -> str:
+    env = os.getenv(name)
+    assert env is not None
+    assert len(env) > 0
+    return env
 
-assert isinstance(github_access_token, str)
-assert isinstance(local_repo_path, str)
-assert isinstance(github_repo, str)
+GITHUB_ACCESS_TOKEN = get_env('GITHUB_ACCESS_TOKEN')
+LOCAL_REPO_PATH = get_env('LOCAL_REPO_PATH')
+GITHUB_REPO = get_env('GITHUB_REPO')
 
-GITHUB_ACCESS_TOKEN = github_access_token
-LOCAL_REPO_PATH = local_repo_path
-GITHUB_REPO = github_repo
+REVIEWERS= get_env('REVIEWERS').split(',')
+BRANCH_PREFIX= get_env('BRANCH_PREFIX')
 
-
+assert len(REVIEWERS) > 0
+assert len(BRANCH_PREFIX) > 0
